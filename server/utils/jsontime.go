251ad6cb2f9c5f846183c265a4c7d0a77300c952
潘小强 <hawkwithwind@gmail.com>
2018-11-02 20:43:12 +0800

    added json time

--- server/utils/jsontime.go
+++ server/utils/jsontime.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	"time"
-	"fmt"
+	"strings"
 )
 
 type JSONTime struct {
@@ -10,6 +10,14 @@ type JSONTime struct {
 }
 
 func (t JSONTime) MarshalJSON() ([]byte, error) {
-	stamp := fmt.Sprintf("\"%s\"", t.Format("Mon Jan _2"))
-	return []byte(stamp), nil
+	return []byte(`"` + t.Time.Format(time.RFC1123) + `"`), nil
+}
+
+func (t *JSONTime) UnmarshalJSON(buf []byte) error {
+	tt, err := time.Parse(time.RFC1123, strings.Trim(string(buf), `"`))
+	if err != nil {
+		return err
+	}
+	t.Time = tt
+	return nil
 }
