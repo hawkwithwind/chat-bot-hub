14efcf575dafb2e90dcf77ebab3a2ed05c10b950
潘小强 <hawkwithwind@gmail.com>
2018-11-02 20:58:11 +0800

    added json time

--- server/utils/jsontime.go
+++ server/utils/jsontime.go
@@ -10,11 +10,11 @@ type JSONTime struct {
 }
 
 func (t JSONTime) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + t.Time.Format(time.RFC1123) + `"`), nil
+	return []byte(`"` + t.Time.Format(time.RFC3339) + `"`), nil
 }
 
 func (t *JSONTime) UnmarshalJSON(buf []byte) error {
-	tt, err := time.Parse(time.RFC1123, strings.Trim(string(buf), `"`))
+	tt, err := time.Parse(time.RFC3339, strings.Trim(string(buf), `"`))
 	if err != nil {
 		return err
 	}
