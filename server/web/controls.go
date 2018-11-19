0c58568f47408aec22e0a5e0ee8615af251a9699
潘小强 <hawkwithwind@gmail.com>
2018-11-19 16:47:47 +0800

    fix web notify url

--- server/web/controls.go
+++ server/web/controls.go
@@ -346,7 +346,7 @@ func (ctx *WebServer) botLogin(w http.ResponseWriter, r *http.Request) {
 	wrapper := o.GRPCConnect(fmt.Sprintf("%s:%s", ctx.Hubhost, ctx.Hubport))
 	defer wrapper.Cancel()
 
-	botnotifypath := fmt.Sprintf("/bots/%s/notify", login)
+	botnotifypath := fmt.Sprintf("/bots/%s/notify", bot.Login)
 
 	loginreply := o.BotLogin(wrapper, &pb.BotLoginRequest{
 		ClientId:   clientId,
