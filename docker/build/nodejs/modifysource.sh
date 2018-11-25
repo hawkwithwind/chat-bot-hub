e5449c79c6dc3f0c30205387d56128757e3273b6
潘小强 <hawkwithwind@gmail.com>
2018-11-25 08:56:47 +0800

    fix

--- docker/build/nodejs/modifysource.sh
+++ docker/build/nodejs/modifysource.sh
@@ -4,7 +4,9 @@ defaultmirror=mirrors.ustc.edu.cn
 
 if [ ${#1} -gt 5 ]; then
     sed -i "s/deb.debian.org/$1/g" /etc/apt/sources.list
+    sed -i "s/security.debian.org/$1/g" /etc/apt/sources.list
 else
     sed -i "s/deb.debian.org/$defaultmirror/g" /etc/apt/sources.list
+    sed -i "s/security.debian.org/$defaultmirror/g" /etc/apt/sources.list
 fi
 
