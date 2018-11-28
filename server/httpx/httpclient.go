dbd06cbaac8c2d833531fa0e10bdf503dd2abdc9
潘小强 <hawkwithwind@gmail.com>
2018-11-28 15:33:06 +0800

    fix content type

--- server/httpx/httpclient.go
+++ server/httpx/httpclient.go
@@ -202,6 +202,8 @@ func RestfulCall(req *RestfulRequest) (*RestfulResponse, error) {
 			nreq.Header.Set(k, v)
 		}
 
+		fmt.Printf("[HTTPX] nreq %v", nreq)
+
 		if nresp, err = client.Do(nreq); err != nil {
 			return nil, err
 		}
