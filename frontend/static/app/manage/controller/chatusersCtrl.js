093c4a06ed67ea865bc715b6c5bf0e4d5ac56f22
Peter-Pan <hawkwithwind@gmail.com>
2019-02-15 19:24:47 +0800

    added contact list on web

--- frontend/static/app/manage/controller/chatusersCtrl.js
+++ frontend/static/app/manage/controller/chatusersCtrl.js
@@ -6,33 +6,25 @@
   function chatusersCtrl($scope, $modal, toastr,
 			 buildModel, buildPromise, tools, buildModelResId) {
     $scope.body = {}
-
-    function jump(i) {
-      buildPromise(
-        buildModel('chatusers'),
-        {
-          'page': i-1,
-          'pagesize': $scope.paging.pagesize ? $scope.paging.pagesize : 100,
-        })
-        .then((data) => {
-          $scope.initView(data)
-        })
-    }
     
-    $scope.initView = (data) => {
-      $scope.body = data.body
-      $scope.paging = data.paging
-      $scope.paging.pagerange = []      
-      for (var i=0;i<$scope.paging.pagecount;i++) {
-	$scope.paging.pagerange.push(i+1)
+    $scope.initView = () => {
+      $scope.paging.jump = (i) => {
+        buildPromise(
+          buildModel('chatusers'),
+          {
+            'page': i-1,
+            'pagesize': $scope.paging.pagesize ? $scope.paging.pagesize : 100,
+          })
+          .then((data) => {
+            $scope.body = data.body
+            $scope.paging = data.paging
+            $scope.paging.pagerange = []      
+            for (var i=0;i<$scope.paging.pagecount;i++) {
+	      $scope.paging.pagerange.push(i+1)
+            }
+          })
       }
 
-      $scope.paging.jump = jump
-    }
-
-    $scope.paging.jump = jump
-    
-    $scope.refresh = () => {
       buildPromise(buildModel('consts'))
 	.then((data) => {
 	  $scope.consts = data.body
@@ -40,7 +32,7 @@
 	})
     }
 
-    $scope.refresh()
+    $scope.initView()
   }  
 })()
 
