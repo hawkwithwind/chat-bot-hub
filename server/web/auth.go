717481610521a5728bf010f3cc92b8f5cea97739
Peter-Pan <hawkwithwind@gmail.com>
2019-04-10 09:33:11 +0800

    added refresh token

--- server/web/auth.go
+++ server/web/auth.go
@@ -18,6 +18,7 @@ const (
 	SDK     string = "SDK"
 	USER    string = "USER"
 	SDKCODE string = "sdkbearer"
+	REFRESHCODE string = "refresh"
 )
 
 type User struct {
@@ -83,6 +84,34 @@ func (o *ErrorHandler) authorize(s string, name string, secret string) string {
 	return o.generateToken(s, name, "", secret, time.Now().Add(time.Hour*24*7))
 }
 
+func (o *ErrorHandler) genSdkToken(web *WebServer, accountName string, expires time.Duration, refreshExpires time.Duration) map[string]interface{} {
+	expireAt := time.Now().Add(expires)
+	refreshExpireAt := time.Now().Add(refreshExpires)
+	
+	account := o.GetAccountByName(web.db.Conn, accountName)
+	if o.Err != nil {
+		return map[string]interface{}{}
+	}
+	if account == nil {
+		o.Err = fmt.Errorf("account %s not found", accountName)
+		return map[string]interface{}{}
+	}
+	
+	tokenString := o.generateToken(web.Config.SecretPhrase, account.AccountName, SDKCODE, account.Secret, expireAt)
+	refreshToken := o.generateToken(web.Config.SecretPhrase, account.AccountName, REFRESHCODE, account.Secret, refreshExpireAt)
+
+	if o.Err != nil {
+		return map[string]interface{}{}
+	}
+	
+	return map[string]interface{}{
+		"sdkName": SDKCODE,
+		"token":   tokenString,
+		"refreshToken": refreshToken,
+		"expireAt": utils.JSONTime{Time: expireAt},
+	}
+}
+
 func (ctx *WebServer) sdkToken(w http.ResponseWriter, req *http.Request) {
 	o := &ErrorHandler{}
 	defer o.WebError(w)
@@ -95,13 +124,54 @@ func (ctx *WebServer) sdkToken(w http.ResponseWriter, req *http.Request) {
 		o.Err = fmt.Errorf("context[login] should be string but [%T]%v", login, login)
 	}
 
-	account := o.GetAccountByName(ctx.db.Conn, accountName)
-	tokenstring := o.generateToken(ctx.Config.SecretPhrase, account.AccountName, "sdkbearer", account.Secret, time.Now().Add(time.Hour*24*365))
+	o.ok(w, "", o.genSdkToken(ctx, accountName, time.Hour*24*7, time.Hour*24*30))
+}
 
-	o.ok(w, "", map[string]interface{}{
-		"sdkName": SDKCODE,
-		"token":   tokenstring,
+func (ctx *WebServer) refreshToken(w http.ResponseWriter, req *http.Request) {
+	o := &ErrorHandler{}
+	defer o.WebError(w)
+
+	bearerToken := req.Header.Get("X-AUTHORIZE-REFRESH")
+	clientType := req.Header.Get("X-CLIENT-TYPE")
+
+	var token *jwt.Token
+	token, o.Err = jwt.Parse(bearerToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("解析令牌出错")
+		}
+		return []byte(ctx.Config.SecretPhrase), nil
 	})
+	if token.Valid {
+		var user User
+		utils.DecodeMap(token.Claims, &user)
+
+		if o.AccountValidateSecret(ctx.db.Conn, user.AccountName, user.Secret) {
+			if user.ExpireAt.Before(time.Now()) {
+				o.deny(w, "身份令牌已过期")
+				return
+			} else {
+				if user.SdkCode != REFRESHCODE {
+					o.deny(w, "不支持的令牌类型")
+					return
+				}
+
+				if clientType != SDK {
+					o.deny(w, "不支持的用户类型")
+					return
+				}
+
+				// pass validate
+				o.ok(w, "", o.genSdkToken(ctx, user.AccountName, time.Hour*24*7, time.Hour*24*30))
+				return
+			}
+		} else {
+			o.deny(w, "身份令牌未验证通过")
+			return
+		}
+	} else {
+		o.deny(w, "身份令牌无效")
+		return
+	}
 }
 
 func (ctx *WebServer) login(w http.ResponseWriter, req *http.Request) {
