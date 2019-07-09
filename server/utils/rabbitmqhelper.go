75c1ab147f85bf4a807ba6faea3eee7ae18ca7f8
Peter-Pan <hawkwithwind@gmail.com>
2019-07-09 18:20:55 +0800

    tune rabbitmq reconnect logic

--- server/utils/rabbitmqhelper.go
+++ server/utils/rabbitmqhelper.go
@@ -94,7 +94,7 @@ func (w *RabbitMQWrapper) Send(queue string, body string) error {
 	}
 
 	o := &ErrorHandler{}
-	if w.mqChannel != nil && w.lastActive.Add(5 * time.Minute).Before(time.Now()) {
+	if w.mqChannel != nil && w.lastActive.Add(2 * time.Minute).Before(time.Now()) {
 		w.mqChannel.Close()
 		w.mqChannel = nil
 	}
@@ -116,6 +116,8 @@ func (w *RabbitMQWrapper) Send(queue string, body string) error {
 			Body:         []byte(body),
 		})
 	if err != nil {
+		w.mqConn.Close()
+		w.mqConn = nil
 		return err
 	}
 
