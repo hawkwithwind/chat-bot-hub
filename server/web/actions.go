48ecefdf37a5b1973d897acfb19b3b2a3dda77c9
Peter-Pan <hawkwithwind@gmail.com>
2019-07-12 19:02:09 +0800

    hot fix

--- server/web/actions.go
+++ server/web/actions.go
@@ -868,12 +868,14 @@ func (ctx *WebServer) processBotNotify(botId string, eventType string, bodystr s
 					}
 
 					if len(foundms) == 0 {
-						if tag, ok := ctx.Config.Fluent.Tags["moment"]; ok {
-							if err := ctx.fluentLogger.Post(tag, m); err != nil {
-								ctx.Error(err, "push moment to fluentd failed")
+						if ctx.fluentLogger != nil {
+							if tag, ok := ctx.Config.Fluent.Tags["moment"]; ok {
+								if err := ctx.fluentLogger.Post(tag, m); err != nil {
+									ctx.Error(err, "push moment to fluentd failed")
+								}
+							} else {
+								ctx.Error(fmt.Errorf("config.fluent.tags.moment not found"), "push moment to fluentd failed")
 							}
-						} else {
-							ctx.Error(fmt.Errorf("config.fluent.tags.moment not found"), "push moment to fluentd failed")
 						}
 					}
 
