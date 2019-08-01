d8088e6490190eecee187a7a0339309645c51ef3
haoda <haoda@bolo.me>
2019-08-01 11:22:20 +0800

    开启 websocket 压缩

--- server/streaming/ws_server.go
+++ server/streaming/ws_server.go
@@ -17,6 +17,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+	EnableCompression: true,
 }
 
 func (server *Server) validateToken(token string) (*utils.AuthUser, error) {
