b96b0a62f68bb580c0e34db890d80b417f4df829
Peter-Pan <hawkwithwind@gmail.com>
2019-08-12 18:53:42 +0800

    fix acceptuser on page

--- frontend/static/app/manage/controller/botslistCtrl.js
+++ frontend/static/app/manage/controller/botslistCtrl.js
@@ -347,7 +347,7 @@
           "name": "EncryptUserName",
           "display": "EncryptUserName",
           "type": "string",
-          "required": false,
+          "required": true,
         },
         commonParams.ticket,
         {
